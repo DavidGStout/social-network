3aa163c16de8b038195b5614952b57809bc116a1
David Stout <davidgstout63@gmail.com>
2019-01-28 17:57:11 -0500

    Connected database to PHP.

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
+<?php
+	$con = mysqli_connect("localhost","root","", "social");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+
+	$query = mysqli_query($con, "INSERT INTO test VALUES('', 'david')");
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Social Network</title>
+	<title>Swirlfeed Social Network</title>
 </head>
 <body>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 </body>
 </html>
