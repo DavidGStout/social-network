bf9f68095579a1741c1a3b13fe4eee03062b03c9
David Stout <davidgstout63@gmail.com>
2019-02-18 07:07:39 -0500

    Cleaned up broken files. Added holder text.

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
-<?php
-	$con = mysqli_connect("localhost","root","", "social");
-
-	if(mysqli_connect_errno()) {
-		echo "Failed to connect: " . mysqli_connect_errno();
-	}
-
-	$query = mysqli_query($con, "INSERT INTO test VALUES('', 'david')");
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Swirlfeed Social Network</title>
+	<title></title>
 </head>
 <body>
-
-	<h1>Test Feb 4</h1>
-
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+	<h1>lorem feb 18</h1>
 </body>
 </html>
