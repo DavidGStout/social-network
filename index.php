5e206c63ad3780dd1c021a1ee13ff3144272ff2f
David Stout <davidgstout63@gmail.com>
2019-02-19 17:12:16 -0500

    Added register page.

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
+<?php
+
+	$con = mysqli_connect("localhost", "root", "", "social");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . (mysqli_connect_errno());
+	}
+
+	$query = mysqli_query($con, "INSERT INTO test VALUES('', 'David')");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-	<h1>lorem feb 18</h1>
+	<h1>lorem feb 19</h1>
 </body>
 </html>
