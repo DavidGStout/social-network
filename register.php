a1abd97767615b8e89d5acb717917d545ceb2716
David Stout <davidgstout63@gmail.com>
2019-02-19 17:41:18 -0500

    Added values from the form functionality. section 2.7

--- register.php
+++ register.php
@@ -1,4 +1,54 @@
-<!DOCTYPE html>
+<?php
+
+	$con = mysqli_connect("localhost", "root", "", "social");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . (mysqli_connect_errno());
+	}
+
+	//Declaring variables to prevent errors
+	$fname = ""; //First name
+	$lname = ""; //Last name
+	$em = ""; //Email
+	$em2 = ""; //Email 2
+	$password = ""; //Password
+	$password2 = ""; //Password 2
+	$date = ""; //Sign up date
+	$error_array = "";
+
+	if(isset($_POST['register_button'])) {
+
+	//Registration form values
+
+	//First name
+	$fname = strip_tags($_POST['reg_fname']); //Remove HTML tags
+	$fname = str_replace(' ', '', $fname); //Remove spaces
+	$fname = ucfirst(strtolower($fname)); //Uppercase first letter
+
+	//Last name
+	$lname = strip_tags($_POST['reg_lname']); //Remove HTML tags
+	$lname = str_replace(' ', '', $lname); //Remove spaces
+	$lname = ucfirst(strtolower($lname)); //Uppercase first letter
+
+	//Email
+	$em = strip_tags($_POST['reg_email']); //Remove HTML tags
+	$em = str_replace(' ', '', $em); //Remove spaces
+	$em = ucfirst(strtolower($em)); //Uppercase first letter
+
+	//Email 2
+	$em2 = strip_tags($_POST['reg_email2']); //Remove HTML tags
+	$em2 = str_replace(' ', '', $em2); //Remove spaces
+	$em2 = ucfirst(strtolower($em2)); //Uppercase first letter
+
+	//Password
+	$password = strip_tags($_POST['reg_password']); //Remove HTML tags
+	$password2 = strip_tags($_POST['reg_password2']); //Remove HTML tags
+
+	$date = date("Y-m-d"); //Current date.
+}
+
+?>
+
 <html>
 <head>
 	<title></title>
